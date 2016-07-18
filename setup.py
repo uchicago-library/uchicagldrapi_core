8915f2e96873e929c3e5ead2b8efd9cfbdeb1b30
Brian Balsamo <balsamo@uchicago.edu>
2016-07-18 16:51:49 -0500

    basic setup script and .gitignore added.

--- setup.py
+++ setup.py
@@ -1,1 +1,27 @@
-print("#TODO")
+from setuptools import setup
+from setuptools import find_packages
+
+def readme():
+    with open("README.md", 'r') as f:
+        return f.read()
+
+setup(
+    name = "uchicagoldrapicore",
+    description = "The UChicago LDR API core package.",
+    long_description = readme(),
+    version = "0.0.1dev",
+    author = "Brian Balsamo, Tyler Danstrom",
+    author_email = "balsamo@uchicago.edu, tdanstrom@uchicago.edu",
+    packages = find_packages(
+        exclude = [
+            "build",
+            "bin",
+            "dist",
+            "tests",
+            "uchicagoldrapicore.egg-info"
+        ]
+    ),
+    install_requires = [
+        "flask"
+    ]
+)
