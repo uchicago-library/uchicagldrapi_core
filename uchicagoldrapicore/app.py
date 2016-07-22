b90031998e209f4fd42ed29f76ee9e3f70c5cdea
Tyler Danstrom <tdanstrom@uchicago.edu>
2016-07-22 11:03:22 -0500

    added after_request to app.py
    As a balm to allow for testing javascript interface connection to hr api
    over localhost:5000 I added headers Access-Control-Allow-Origin

--- uchicagoldrapicore/app.py
+++ uchicagoldrapicore/app.py
@@ -36,3 +36,10 @@ try:
     app.register_blueprint(hr_bp)
 except Exception:
     pass
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response    
